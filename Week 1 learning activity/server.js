597f2718d38b84a5a844b83c5cfba020d8979a2c
kaden530 <bro22001@byui.edu>
2024-01-12 16:29:46 -0700

    added database

--- Week 1 learning activity/server.js
+++ Week 1 learning activity/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const routes = express();
- 
-routes.get('/', require('./index.html'));
- 
-const port =3000
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const professionalRoutes = require('./professional');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
