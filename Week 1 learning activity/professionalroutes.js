8b725a712af8affb43e37302e6b25f3f99fb5822
kaden530 <bro22001@byui.edu>
2024-01-23 14:17:34 -0700

    Started week 1 project

--- Week 1 learning activity/professionalroutes.js
+++ Week 1 learning activity/professionalroutes.js
@@ -5,6 +5,6 @@ const professionalController = require('./professional');
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/', professionalController.getData);
+router.get('./professional', professionalController.getData);
 // localhost:8080/professional/
 module.exports = router;
