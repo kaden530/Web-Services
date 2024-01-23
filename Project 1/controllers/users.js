c205b2ae1a130f2c7aa0d6ae82286115857ad096
kaden530 <bro22001@byui.edu>
2024-01-23 16:08:47 -0700

    week 1 attempt 1

---
+++ Project 1/controllers/users.js
@@ -0,0 +1,23 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async(req, res) => {
+    const result = await mongodb.getDb.db().collection('Contacts').find();
+    result.toArray().then((users) =>{
+        res.setHeader('Content-Type','application/json');
+        res.status(200).json(users);
+    });
+};
+const getSingle = async(req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb.db().collection('Contacts').find({_id: userId});
+    result.toArray().then((users) =>{
+        res.setHeader('Content-Type','application/json');
+        res.status(200).json(users[0]);
+    });
+};
+
+module.exports ={
+    getAll,
+    getSingle
+};
--- Project 1/server.js
+++ Project 1/server.js
@@ -1,21 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const mongodb = require('./connect');
-const professionalRoutes = require('./professionalroutes');
+const mongodb = require('./data/database');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('./professional', professionalRoutes);
+app.use('/', require('./routes' ));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
